e6ad9a6f232ae89847566d1698e0a5fce0241bfc
Ido <idobenoun@gmail.com>
2024-01-23 19:55:50 +0200

    Update

---
+++ src/core/config.js
@@ -0,0 +1,23 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAL0c1rZ9JLMIcobBeuc8YpZPP-AC_GE84",
+  authDomain: "tripy-e6333.firebaseapp.com",
+  projectId: "tripy-e6333",
+  storageBucket: "tripy-e6333.appspot.com",
+  messagingSenderId: "551607313233",
+  appId: "1:551607313233:web:055f2cd3ab1e7916f36970",
+  measurementId: "G-6MPNKQGPBX"
+};
+
+
+// Replace with your own IDs! follow the guides here:
+// https://docs.expo.io/versions/latest/sdk/google/#using-it-inside-of-the-expo-app
+export const ANDROID_GOOGLE_CLIENT_ID =
+  '826066565685-lj072iilquvn7o2rsk44qh509v87kien.apps.googleusercontent.com'
+export const IOS_GOOGLE_CLIENT_ID =
+  '878215484396-5hiqfjetgvbdck03l0jepdgku6u64erm.apps.googleusercontent.com'
+
+// Replace with your own facebook app ID.
+// You can find more information how to generate one here:
+// https://docs.expo.io/versions/latest/sdk/facebook/#registering-your-app-with-facebook
+// export const FACEBOOK_APP_ID = '873805167813153'
